e76b735bcd43ea111cc7e08004eebc82e1b04288
Randal Hanford <rhanford@gmail.com>
2017-05-12 16:33:43 -0700

    Added additional code to show browser window

--- app/main.js
+++ app/main.js
@@ -1,1 +1,8 @@
 const {app, BrowserWindow} = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () =>  {
+    console.log('hello from Electron');
+    mainWindow = new BrowserWindow();
+})
