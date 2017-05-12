9e6d61efe06d11fda0d22512d39d44c461d9ef78
Randal Hanford <rhanford@gmail.com>
2017-05-12 16:46:01 -0700

    Change main to load the url which is index.html

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ const {app, BrowserWindow} = require('electron');
 let mainWindow = null;
 
 app.on('ready', () =>  {
-    console.log('hello from Electron');
+    console.log('hello from Electron');    
     mainWindow = new BrowserWindow();
+    mainWindow.webContents.loadURL(`file://${__dirname}/index.html`);
 })
