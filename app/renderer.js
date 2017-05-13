ddba0f317cb08d92ea10db4269039a9f8be2a856
Randal Hanford <rhanford@gmail.com>
2017-05-12 19:25:40 -0700

    Added the parsing responses

--- app/renderer.js
+++ app/renderer.js
@@ -1,3 +1,5 @@
+const parser = new DOMParser();
+
 const linksSection = document.querySelector('.links');
 const errorMessage = document.querySelector('.error-message');
 const newLinkForm = document.querySelector('.new-link-form');
@@ -12,4 +14,19 @@ newLinkUrl.addEventListener('keyup', () => {
 const clearForm = () => {
     newLinkUrl.value = null;
 }
+const parseResponse = (text) =>{
+    return parser.parseFromString(text, 'text/html');
+}
+const findTitle = (nodes) => {
+    return nodes.querySelector('title').innerText;
+}
 
+newLinkForm.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    const url = newLinkUrl.value;
+
+    fetch(url)
+        .then(response => response.text())
+        .then(parseResponse)
+        .then(findTitle)
+})
